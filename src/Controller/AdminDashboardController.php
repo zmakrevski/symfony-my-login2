af146d534bf229b68997b4c8067ed42d02eaa7ca
Makrevski Zoran <zoran@makrevski.com>
2023-06-19 01:18:42 +0300

    Modify route

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminDashboardController extends AbstractController
 {
-    #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
+    #[Route('/admin', name: 'admin_dashboard')]
     public function index(): Response
     {
         return $this->render('admin_dashboard/index.html.twig', [
