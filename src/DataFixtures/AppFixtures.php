11e8c6cfc2df43dd65fe7bed3966d03341611b79
Makrevski Zoran <zoran@makrevski.com>
2023-06-18 17:27:56 +0300

    Modify method

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,12 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        UserFactory::createOne([
+            'password' => 'tada',
+            'email' => 'zoran@makrevski.com',
+            'roles' => ['ROLE_USER', 'ROLE_ADMIN']
+        ]);
+        UserFactory::createMany(10);
 
         $manager->flush();
     }
